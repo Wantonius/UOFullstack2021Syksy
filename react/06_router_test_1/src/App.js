01dfa5f6ba7f542eed913022e9610011144fa758
Erno Hentonen <erno.hentonen@opiframe.com>
2021-10-27 14:14:01 +0300

    router

---
+++ react/06_router_test_1/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Switch,Link,Route} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Secret from './Secret';
+class App extends React.Component {
+  render() {
+	return (
+		<div className="App">
+			<ul style={{listStyleType:"none"}}>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+			</ul>
+			<hr/>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route path="/about" component={About}/>
+				<Route path="/secret" component={Secret}/>
+			</Switch>
+		</div>
+	);
+  }
+}
+
+export default App;
---
+++ react/06_router_test_1/src/Secret.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Secret extends React.Component {
+	render() {
+		return (
+			<h1>Welcome to secret page</h1>
+		)
+	}
+}
