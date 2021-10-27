548daed66c222b6a2884ef45a4607328d06546e6
Erno Hentonen <erno.hentonen@opiframe.com>
2021-10-27 14:48:10 +0300

    routing tst 1

--- react/06_router_test_1/src/About.js
+++ react/06_router_test_1/src/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class About extends React.Component {
 	render() {
 		return (
-			<h1>This is a React router test app</h1>
+			<div>
+				<h1>This is a React router test app</h1>
+				<button onClick={() => this.props.history.push("/secret")}> Go to secret page!</button>
+			</div>
 		)
 	}
 }
