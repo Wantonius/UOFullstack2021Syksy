bd979ea9d596895bab2a222cdc20faeb8a375b0a
Erno Hentonen <erno.hentonen@opiframe.com>
2021-10-28 10:51:35 +0300

    update

--- react/50_shoppingapp/01_stage/backend/server.js
+++ react/50_shoppingapp/01_stage/backend/server.js
@@ -38,6 +38,21 @@ app.delete("/api/shopping/:id",function(req,res) {
 	return res.status(200).json({message:"success"});
 })
 
+app.put("/api/shopping/:id",function(req,res) {
+	let tempId = parseInt(req.params.id,10);
+	let item = {
+		...req.body,
+		id:tempId
+	}
+	for(let i=0;i<database.length;i++) {
+		if(database[i].id === tempId) {
+			database.splice(i,1,item);
+			return res.status(200).json({message:"success"});
+		}
+	}
+	return res.status(404).json({message:"not found"});
+})
+
 app.listen(port);
 
 console.log("Running in port "+port);
