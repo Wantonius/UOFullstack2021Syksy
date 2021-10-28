a5f044269e0d722247b1acc98db24ba95cdc6692
Erno Hentonen <erno.hentonen@opiframe.com>
2021-10-28 12:42:47 +0300

    stage 1

---
+++ react/50_shoppingapp/01_stage/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoppingForm from './components/ShoppingForm';
+
+function App() {
+  return (
+    <div className="App">
+		<ShoppingForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/50_shoppingapp/01_stage/frontend/src/components/ShoppingForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Form,Button} from 'semantic-ui-react';
+
+export default class ShoppingForm extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			type:"",
+			count:0,
+			price:0
+		}
+	}
+	
+	onChange = (event) => {
+		this.setState({
+			[event.target.name]:event.target.value
+		})
+	}
+	
+	onSubmit = (event) => {
+		event.preventDefault();
+		let item = {
+			...this.state
+		}
+		this.props.addToList(item);
+		this.setState({
+			type:"",
+			count:0,
+			price:0	
+		})
+	}
+	
+	render() {
+		return(
+		<div style={{
+			width:500,
+			margin:"auto",
+			backgroundColor:"lightgreen"
+		}}>
+			<Form onSubmit={this.onSubmit}>
+				<Form.Field>
+					<label htmlFor="type">Item Type</label>
+					<input type="text"
+							name="type"
+							onChange={this.onChange}
+							value={this.state.type}/>
+				</Form.Field>
+				<Form.Field>
+					<label htmlFor="count">Count</label>
+					<input type="number"
+							name="count"
+							onChange={this.onChange}
+							value={this.state.count}/>
+				</Form.Field>
+				<Form.Field>
+					<label htmlFor="price">Price</label>
+					<input type="number"
+							name="price"
+							step="0.01"
+							onChange={this.onChange}
+							value={this.state.price}/>
+				</Form.Field>
+				<Button type="submit">Add</Button>
+			</Form>
+		</div>
+		)
+		
+	}
+}
