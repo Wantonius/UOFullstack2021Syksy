1724332a10c1178910b7a065fe5b9b54eb96799d
Erno Hentonen <erno.hentonen@opiframe.com>
2021-11-03 12:40:21 +0200

    register

--- react/50_shoppingapp/02_stage_login/backend/server.js
+++ react/50_shoppingapp/02_stage_login/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes/apiroutes");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 let app = express();
 
@@ -11,6 +12,32 @@ app.use(express.json());
 let registeredUsers = [];
 let loggedSessions = [];
 const time_to_live_diff = 3600000;
+
+//HELPERS
+
+createToken = () => {
+	let token = crypto.randomBytes(128);
+	return token.toString("hex");
+}
+
+isUserLogged = (req,res,next) => {
+	if(!req.headers.token) {
+		return res.status(403).json({message:"Forbidden"})
+	}
+	for(let i=0;i<loggedSessions.length;i++) {
+		if(req.headers.token === loggedSessions[i].token) {
+		let now = Date.now();
+		if(now > loggedSessions[i].ttl) {
+			loggedSessions.splice(i,1);
+			return res.status(403).json({message:"Forbidden"})
+		}
+		loggedSessions[i].ttl = now + time_to_live_diff;
+		req.session = {};
+		req.session.user = loggedSessions[i].user;
+		return next();
+	}
+	return res.status(403).json({message:"Forbidden"})
+}
 
 //LOGIN API
 
@@ -43,9 +70,45 @@ app.post("/register",function(req,res) {
 	})
 })
 
+app.post("/login",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(req.body.username.length < 4 ||req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request"});	
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(registeredUsers[i].username === req.body.username) {
+			bcrypt.compare(req.body.password,registeredUsers[i].password, function(err,success) {
+				if(err) {
+					return res.status(500).json({message:"Server error"})
+				}
+				if(!success) {
+					return res.status(403).json({message:"Forbidden"})
+				}
+				let token = createToken();
+				let now = Date.now();
+				let session = {
+					user:req.body.username,
+					ttl:now+time_to_live_diff,
+					token:token
+				}
+				loggedSessions.push(session);
+				return res.status(200).json({token:token})
+			})
+			return;
+		}
+	}
+	return res.status(403).json({message:"Forbidden"})
+})
+
+
 let port = process.env.PORT || 3001
 
-app.use("/api",routes);
+app.use("/api",isUserLogged,routes);
 
 app.listen(port);
 
