2aba4643649f105b8d6a45eb4fe10192b202213a
Erno Hentonen <erno.hentonen@opiframe.com>
2021-11-03 10:43:43 +0200

    register

--- react/50_shoppingapp/02_stage_login/backend/server.js
+++ react/50_shoppingapp/02_stage_login/backend/server.js
@@ -5,11 +5,42 @@ let app = express();
 
 app.use(express.json());
 
-app.use((req,res,next) => {
-	console.log("Hi, I am a middleware filter!");
-	return next();
-});
+//LOGIN DATABASES
 
+let registeredUsers = [];
+let loggedSessions = [];
+const time_to_live_diff = 3600000;
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(req.body.username.length < 4 ||req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request"});	
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({message:"Username is already in use"})
+		}
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			return res.status(500).json({message:"Server error"})
+		}
+		let user = {
+			username:req.body.username,
+			password:hash
+		}
+		registeredUsers.push(user);
+		console.log(registeredUsers);
+		return res.status(201).json({message:"New user created!"});
+	})
+})
 
 let port = process.env.PORT || 3001
 
