663250bcf359475b7a7b4097736aedab92bf1e18
Erno Hentonen <erno.hentonen@opiframe.com>
2021-11-24 14:05:08 +0200

    complex custom hooks

--- react/50_shoppingapp/05_stage_functional/frontend/src/stagemanager/provider/StateProvider.js
+++ react/50_shoppingapp/05_stage_functional/frontend/src/stagemanager/provider/StateProvider.js
@@ -3,16 +3,152 @@ import StateContext from '../context/StateContext';
 import ActionContext from '../context/ActionContext';
 import {ActionConstants} from '../actionconstants';
 
-const initialState = {
-	loading:false,
-	error:"",
-	list:[],
-	token:"",
-	isLogged:false
+const getInitialState = () => {
+	if(sessionStorage.getItem("state")) {
+	 	let state = JSON.parse(sessionStorage.getItem("state"));
+		return state;
+	} else {
+		return {
+			loading:false,
+			error:"",
+			list:[],
+			token:"",
+			isLogged:false
+		}
+	}
+}
+
+const initialState = getInitialState();
+
+const saveToStorage = (state) => {
+	sessionStorage.setItem("state",JSON.stringify(state));
 }
 
 const reducer = (state,action) => {
-	
+	let tempState = {}
+	switch(action.type) {
+		case ActionConstants.LOADING: 
+			return {
+				...state,
+				loading:true,
+				error:""
+			}
+		case ActionConstants.STOP_LOADING:
+			return {
+				...state,
+				loading:false,
+				error:""
+			}
+		case ActionConstants.REGISTER_SUCCESS:
+			tempState = {
+				...state,
+				error:"Register success!"
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.REGISTER_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.LOGIN_SUCCESS:
+			tempState = {
+				...state,
+				error:"",
+				isLogged:true,
+				token:action.token
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.LOGIN_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.LOGOUT_SUCCESS:
+			tempState = {
+				loading:false,
+				list:[],
+				token:"",
+				isLogged:false,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		 case ActionConstants.LOGOUT_FAILED:
+			tempState = {
+				loading:false,
+				list:[],
+				token:"",
+				isLogged:false,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.FETCH_LIST_SUCCESS:
+			tempState = {
+				...state,
+				list:action.list,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.FETCH_LIST_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.ADD_ITEM_SUCCESS:
+			tempState = {
+				...state,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.ADD_ITEM_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.REMOVE_ITEM_SUCCESS:
+			tempState = {
+				...state,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;	
+		case ActionConstants.REMOVE_ITEM_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case ActionConstants.EDIT_ITEM_SUCCESS:
+			tempState = {
+				...state,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;	
+		case ActionConstants.EDIT_ITEM_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		default:
+			return state;
+	}
 }
 
 const StateProvider = (props) => {
