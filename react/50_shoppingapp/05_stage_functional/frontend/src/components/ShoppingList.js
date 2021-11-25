b1207bdef3e4b2d21a5fe5a7dc75e11a70ecc101
Erno Hentonen <erno.hentonen@opiframe.com>
2021-11-25 13:32:19 +0200

    loginpage

--- react/50_shoppingapp/05_stage_functional/frontend/src/components/ShoppingList.js
+++ react/50_shoppingapp/05_stage_functional/frontend/src/components/ShoppingList.js
@@ -1,81 +1,85 @@
-import React from 'react';
+import {useState} from 'react';
 import {Table} from 'semantic-ui-react';
 import Row from './Row';
 import RemoveRow from './RemoveRow';
 import EditRow from './EditRow';
+import useAppState from '../statemanager/useappstate';
+import useAction from '../statemanager/useaction';
 
-export default class ShoppingList extends React.Component {
+const ShoppingList = (props) =>  {
 	
-	constructor(props) {
-		super(props);
-		this.state = {
-			removeIndex:-1,
-			editIndex:-1
-		}
-	}
+	const appstate = useAppState();
 	
-	changeToRemoveMode = (index) => {
-		this.setState({
+	const {removeItem,editItem} = useAction();
+	
+	const [state,setState] = useState({
+		removeIndex:-1,
+		editIndex:-1
+	})
+	
+	const changeToRemoveMode = (index) => {
+		setState({
 			removeIndex:index,
 			editIndex:-1
 		})
 	}
 
-	changeToEditMode = (index) => {
-		this.setState({
+	const changeToEditMode = (index) => {
+		setState({
 			removeIndex:-1,
 			editIndex:index
 		})
 	}
 	
-	cancel = () => {
-		this.setState({
+	const cancel = () => {
+		setState({
 			removeIndex:-1,
 			editIndex:-1
 		})
 	}
 	
-	removeFromList = (id) => {
-		this.props.removeFromList(id);
-		this.cancel();
+	const removeFromList = (id) => {
+		removeItem(id);
+		cancel();
 	}
 	
-	editItem = (item) => {
-		this.props.editItem(item);
-		this.cancel();
+	const edit = (item) => {
+		editItem(item);
+		cancel();
 	}
 	
-	render() {
-		let items = this.props.list.map((item,index) => {
-			if(this.state.editIndex === index) {
-				return (<EditRow key={item._id} item={item}
-					editItem={this.editItem}
-					cancel={this.cancel} />)
-			}
-			if(this.state.removeIndex === index) {
-				return (<RemoveRow key={item._id} item={item}
-					cancel={this.cancel} removeFromList={this.removeFromList}/>)
-			}
-			return (
-				<Row key={item._id} item={item}
-					index={index} 
-					changeToRemoveMode={this.changeToRemoveMode}
-					changeToEditMode={this.changeToEditMode}/>
-			)
-		})
-		return(
-			<Table striped>
-				<Table.Header>
-					<Table.HeaderCell>Item Type</Table.HeaderCell>
-					<Table.HeaderCell>Count</Table.HeaderCell>
-					<Table.HeaderCell>Price</Table.HeaderCell>
-					<Table.HeaderCell>Remove</Table.HeaderCell>
-					<Table.HeaderCell>Edit</Table.HeaderCell>
-				</Table.Header>
-				<Table.Body>
-				{items}
-				</Table.Body>
-			</Table>
-		) 
-	}	
+	let items = appstate.list.map((item,index) => {
+		if(state.editIndex === index) {
+			return (<EditRow key={item._id} item={item}
+				editItem={edit}
+				cancel={cancel} />)
+		}
+		if(state.removeIndex === index) {
+			return (<RemoveRow key={item._id} item={item}
+				cancel={cancel} removeFromList={removeFromList}/>)
+		}
+		return (
+			<Row key={item._id} item={item}
+				index={index} 
+				changeToRemoveMode={changeToRemoveMode}
+				changeToEditMode={changeToEditMode}/>
+		)
+	})
+	return(
+		<Table striped>
+			<Table.Header>
+				<Table.HeaderCell>Item Type</Table.HeaderCell>
+				<Table.HeaderCell>Count</Table.HeaderCell>
+				<Table.HeaderCell>Price</Table.HeaderCell>
+				<Table.HeaderCell>Remove</Table.HeaderCell>
+				<Table.HeaderCell>Edit</Table.HeaderCell>
+			</Table.Header>
+			<Table.Body>
+			{items}
+			</Table.Body>
+		</Table>
+	) 
+		
 }
+
+export default ShoppingList;
