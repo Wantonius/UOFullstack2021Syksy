567e37ac9533c4facd9f587c0ff0a0f9ba2244f6
Erno Hentonen <erno.hentonen@opiframe.com>
2021-11-25 12:39:04 +0200

    functional changes

--- react/50_shoppingapp/05_stage_functional/frontend/src/components/LoginPage.js
+++ react/50_shoppingapp/05_stage_functional/frontend/src/components/LoginPage.js
@@ -1,59 +1,60 @@
-import React from 'react';
+import {useState} from 'react';
 import {Form, Button} from 'semantic-ui-react';
+import useAction from '../statemanager/useaction';
 
-export default class LoginPage extends React.Component {
+const LoginPage = (props) => {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			username:"",
-			password:""
-		}
-	}
+	const [state,setState] = useState({
+		username:"",
+		password:""
+	})
 	
-	onChange = (event) => {
-		this.setState({
+	const {login,register} = useAction();
+	
+	const onChange = (event) => {
+		setState({
+			...state,
 			[event.target.name]:event.target.value
 		})
 	}
 	
-	onSubmit = (event) => {
+	const onSubmit = (event) => {
 		let user = {
-			...this.state
+			...state
 		}
 		if(event.target.name === "register") {
-			this.props.register(user);
+			register(user);
 		} else {
-			this.props.login(user);
+			login(user);
 		}
-	}
-	render() {
-		let loginStyle = {
-			width:500,
-			margin:"auto"
-		}
-		return(
-		<div style={loginStyle}>
-			<Form>
-				<Form.Field>
-					<label htmlFor="username">Username</label>
-					<input type="text"
-							name="username"
-							onChange={this.onChange}
-							value={this.state.username}/>
-				</Form.Field>
-				<Form.Field>
-					<label htmlFor="password">Password</label>
-					<input type="password"
-							name="password"
-							onChange={this.onChange}
-							value={this.state.password}/>
-				</Form.Field>				
-				<Button name="register" onClick={this.onSubmit}>Register</Button>
-				<Button name="login" onClick={this.onSubmit}>Login</Button>
-			</Form>
-		</div>
-		)
 	}
 	
+	let loginStyle = {
+		width:500,
+		margin:"auto"
+	}
+	return(
+	<div style={loginStyle}>
+		<Form>
+			<Form.Field>
+				<label htmlFor="username">Username</label>
+				<input type="text"
+						name="username"
+						onChange={onChange}
+						value={state.username}/>
+			</Form.Field>
+			<Form.Field>
+				<label htmlFor="password">Password</label>
+				<input type="password"
+						name="password"
+						onChange={onChange}
+						value={state.password}/>
+			</Form.Field>				
+			<Button name="register" onClick={onSubmit}>Register</Button>
+			<Button name="login" onClick={onSubmit}>Login</Button>
+		</Form>
+	</div>
+	)
 }
+
+export default LoginPage;
